03b0771718c45bd3b06a3db70471f56820c4b563
Alcarian <floa2@msn.com>
2023-06-09 12:23:21 +0200

    mysqlconexion

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,5 @@
 const MessagesModel = require("../models/postModel");
 const mysqlConnection = require("../config/db");
-const mysqlConnection = require("../config/db");
 
 exports.getPosts = async (req, res) => {
   try {
