49e1ec0f1b9087144e817b1a3726d23b81c2764f
Alcarian <floa2@msn.com>
2023-06-09 12:17:52 +0200

    modif url

--- config/DataBase.js
+++ config/DataBase.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 const mysql = require("mysql2");
 
+//url
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
 const mysqlconnection = mysql.createConnection(urlDB);
