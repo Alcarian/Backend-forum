39c121ac24451f71714564ce2d272095ba347d96
Alcarian <floa2@msn.com>
2023-06-13 16:15:02 +0200

    modif db

--- config/DataBase.js
+++ config/DataBase.js
@@ -1,20 +1,23 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 //url
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
 const mysqlconnection = mysql.createConnection(urlDB);
 
-mysqlconnection.connect((err) => {
-  if (err) {
-    console.log(`error connecting: ${err.stack}`);
-  } else {
-    console.log("connecté à la base de donnée forum");
-    console.log(`connected as id ${mysqlconnection.threadId}`);
-  }
-});
+mysqlconnection
+  .getConnection()
+  .then((connection) => {
+    console.log("Connecté à la base de données forum");
+    console.log(`Connected as id ${connection.threadId}`);
+
+    connection.release();
+  })
+  .catch((error) => {
+    console.log(`Erreur de connexion à la base de données: ${error}`);
+  });
 
 module.exports = mysqlconnection;
